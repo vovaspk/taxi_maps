36a0b1aa941f7d6120d637688d2b2ddaadba0b27
Volodymyr_Slipchyk <Volodymyr_Slipchyk@epam.com>
2019-12-13 16:02:09 +0200

    sql for init cars added

---
+++ src/main/resources/sql/add_cars.sql
@@ -0,0 +1,8 @@
+insert into car (id, name, car_status, car_type, location_id)
+values (1, 'Toyota49050', 'FREE', 'ORDINARY', 1),
+values (2, 'Nissan22073', 'FREE', 'ORDINARY', 2),
+values (3, 'Hyundai32785', 'FREE', 'ORDINARY', 3),
+values (4, 'Kia33870', 'FREE', 'ORDINARY', 4),
+values (5, 'Renault25047', 'FREE', 'ORDINARY', 5),
+values (6, 'Suzuki42590', 'FREE', 'ORDINARY', 6),
+values (7, 'Tesla17093', 'FREE', 'ORDINARY', 7);
